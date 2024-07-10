85bbe548381e2216999cc5d56455cab89ff55816
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-10 19:14:30 +0200

    Create Password Generator

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+
+// ask user name
+const UserName= prompt('What is your name?');
+console.log('Name:',UserName);
+
+
+// ask user surname
+const UserSurname = prompt('What is your surname?');
+console.log('Surname:',UserSurname);
+
+//ask users fav colour 
+
+const UserFavColour = prompt('What is your favourite colour?');
+console.log('Surname:',UserFavColour);
+
+//add random number 
+const RandomNumber = 130;
+//generate password 
+
+document.getElementById('password').innerHTML = 'Your new password is ' + UserName + UserSurname + UserFavColour + RandomNumber;
+
+
+
+
+
